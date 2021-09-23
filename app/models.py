9998f8f0503e28b681d821fc881e7256520669fd
TERESIA012 <kingoriteresia@gmail.com>
2021-09-23 23:45:56 +0300

    add imports

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from . import db
+from . import login_manager
+from flask_login import UserMixin
+from werkzeug import generate_password_hash,check_password_hash
+from datetime import datetime
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
+    my_date = db.Column(db.DateTime,default=datetime.utcnow)
+    blogs = db.relationship('Blog', backref ='user', passive_deletes=True,lazy = "dynamic")
+    comments = db.relationship('Comment', backref ='user' , passive_deletes=True,  lazy ="dynamic")
