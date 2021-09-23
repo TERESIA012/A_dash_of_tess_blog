571b02fba05f8b1bd98688fec15c513945419589
TERESIA012 <kingoriteresia@gmail.com>
2021-09-24 00:12:27 +0300

    add class post

--- app/models.py
+++ app/models.py
@@ -15,6 +15,51 @@ class User(UserMixin,db.Model):
     profile_pic_path = db.Column(db.String())
     pass_secure = db.Column(db.String(255))
     password_hash = db.Column(db.String(255))
+    # image_file = db.Column(db.String(255), nullable=False, default='default.jpg')
     my_date = db.Column(db.DateTime,default=datetime.utcnow)
-    blogs = db.relationship('Blog', backref ='user', passive_deletes=True,lazy = "dynamic")
-    comments = db.relationship('Comment', backref ='user' , passive_deletes=True,  lazy ="dynamic")
+    posts = db.relationship('Post', backref ='user',lazy = "dynamic")
+    comment = db.relationship('Comment', backref ='user', lazy ="dynamic")
+    
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+    def delete_comment(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+    @password.setter
+    def password(self, password):
+            self.password_hash = generate_password_hash(password)
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+    
+    
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    my_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'Post title: {self.title}, Date Posted: {self.my_date}, Post Content: {self.content}'
+    
+    
+    
+    
+   
