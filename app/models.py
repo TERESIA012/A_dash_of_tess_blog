bcef376d76b6d0025bd7c1ef9662f5475af67379
TERESIA012 <kingoriteresia@gmail.com>
2021-09-24 01:14:13 +0300

    add process request

--- app/models.py
+++ app/models.py
@@ -9,8 +9,9 @@ class Quote:
     """
     New class to define the class quote
     """
-    def __init__(self,id,quote):
+    def __init__(self,author,id,quote):
         
+        self.author = author
         self.id=id
         self.quote=quote
         
--- app/requests.py
+++ app/requests.py
@@ -1,2 +1,19 @@
 import urllib.request,json
 from .models import Quote
+
+
+
+def process_request(quote_list):
+    """
+    function process_request to fetch the API
+    """
+    quotes=[]
+    for quote in quote_list:
+        author= quote.get('author')
+        quote= quote.get('quote')
+        quotes.append(Quote(author, quote))
+        return quotes
+                          
+
+
+
