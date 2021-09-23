7b7e34df588cf9f75a27614370e3d7b19a1f8532
TERESIA012 <kingoriteresia@gmail.com>
2021-09-24 01:33:11 +0300

    add get_quote function

--- app/requests.py
+++ app/requests.py
@@ -3,7 +3,7 @@ from .models import Quote
 
 
 
-def process_request(quote_list):
+def process_requests(quote_list):
     """
     function process_request to fetch the API
     """
@@ -13,6 +13,23 @@ def process_request(quote_list):
         quote= quote.get('quote')
         quotes.append(Quote(author, quote))
         return quotes
+    
+    
+def get_quote():
+    """
+    Fetches quotes from Api
+    """   
+    url = "http://quotes.stormconsultancy.co.uk/quotes/25"
+    with urllib.request.urlopen(url) as response:
+        quote_list= json.loads(response.read)
+        return process_requests(quote_list)
+    
+    
+    
+    
+    
+        
+        
                           
 
 
