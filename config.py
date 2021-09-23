04fdabcffdb23a092d4faadbde44f1bdd638ff81
TERESIA012 <kingoriteresia@gmail.com>
2021-09-23 23:49:09 +0300

    add configuration

---
+++ config.py
@@ -0,0 +1,54 @@
+import os
+
+class Config:
+    #debug = True
+    SECRET_KEY = 'gaidi'
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    #  email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 525
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    
+    @staticmethod
+    def init_app(app):
+        pass
+    
+    
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")   
+    
+    
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch'  
+    
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch'
+    DEBUG = True  
+    
+    
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig,
+    'test':TestConfig
+}         
