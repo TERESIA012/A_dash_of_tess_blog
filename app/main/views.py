902643e32bef0d35522b42f769a1f5b6fc9c0c74
TERESIA012 <kingoriteresia@gmail.com>
2021-09-24 13:28:57 +0300

    add index function

---
+++ app/main/views.py
@@ -0,0 +1,25 @@
+from app.requests import get_quote
+from flask import render_template, request, redirect, url_for, abort
+from flask.helpers import flash
+from . import main
+from .forms import UpdateProfile
+from ..models import User
+from flask_login import login_required
+from .. import db,photos
+from flask_login import login_required, current_user
+# import markdown2  
+from .forms import PostForm, CommentForm, UpdateProfile
+from ..models import Post, Comment, User, Upvote, Category,Quote,Subscriber
+
+
+@main.route('/')
+@main.route('/home')
+def index():
+    
+    """
+    View root page function that returns the index page and its data
+    """
+    quotes = get_quote()
+    title="Welcome to my blog"
+    
+    return render_template('index.html', quotes=quotes, title=title)
